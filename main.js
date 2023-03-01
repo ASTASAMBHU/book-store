612d5390dfe0368b6bf61d947e4b4971cce601b8
astasambhu mohanty <astasambhu97@gmail.com>
2023-03-01 12:17:06 +0530

    Add Filter and Dropdown filter

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
+const main_wrapper = document.getElementById('main');
+const serchInput = document.getElementById('serchInput');
+const selectDropdown = document.getElementById('selectDropdown');
 
-let main_wrapper = document.getElementById('main');
-
-let serchInput = document.getElementById('serchInput');
-
-
-var query = 'react';
-
+let query = 'react';
 function fetchUrl(query){
-    console.log(query)
-    var fetch_url = 'https://www.googleapis.com/books/v1/volumes?q='+query+'?maxResults=40';
+    const fetch_url = 'https://www.googleapis.com/books/v1/volumes?q='+query+'?maxResults=40';
     return fetch_url;
 }
-
-
-
 
 async function getData(url){
 
     try{
-        console.log(url);
         let response = await fetch(url);
 
         let data = await response.json();
@@ -27,20 +19,15 @@ async function getData(url){
 
     catch{
         console.log('Error');
-    }
-    
-    
+    }   
 }
-
-
-
 
 function appendData(data){
     main_wrapper.innerHTML="";
-    data.items.map(ids=>{
-        let {title,imageLinks,contentVersion,publisher,publishedDate} = ids.volumeInfo;
-        let showYear = new Date(publishedDate).getFullYear();
-        let templateString = `<div class="card" style="width: 18rem;">
+    data.map(ids=>{
+        const {title,imageLinks,contentVersion,publisher,publishedDate} = ids.volumeInfo;
+        const showYear = new Date(publishedDate).getFullYear();
+        const templateString = `<div class="card" style="width: 18rem;">
             <img src="${imageLinks.thumbnail}" class="img-fluid" alt="Card Top Image">
             <div class="card-body p-0">
                 <p class="card-text mt-3 content-version">${contentVersion}</p>
@@ -49,24 +36,37 @@ function appendData(data){
             </div>
             <p class="show-date">${showYear}</p>
         </div>`;
-        main_wrapper.insertAdjacentHTML('beforeend',templateString);
-                
+        main_wrapper.insertAdjacentHTML('beforeend',templateString);          
     });   
 }
 
-
 async function showData(){
     let dataStore = await getData(fetchUrl(query));
-    console.log(dataStore);
-    appendData(dataStore);
+    appendData(dataStore.items);
 }
 showData();
 
-
 serchInput.addEventListener('input',async function(e){
-    var query = e.target.value;
-    console.log(query);    
+    let query = e.target.value;
     let dataStore = await getData(fetchUrl(query));
-    appendData(dataStore);
+    appendData(dataStore.items);
+    
+});
+
+async function filterByYear(data,val){
+    let dataFilter = data.items.filter((currData)=>{
+        const yearShow = new Date(currData.volumeInfo.publishedDate).getFullYear();
+        return yearShow == val; 
+        
+    });
+    return dataFilter;
+}
+
+selectDropdown.addEventListener('change',async function(e){
+    const selectDropdowns = e.target.value;
+    const url = fetchUrl('react');
+    const data = await getData(url);
+    const dataYearfilter = await filterByYear(data,selectDropdowns);
+    appendData(dataYearfilter);
 });
 
